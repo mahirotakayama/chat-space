5450c18531fc8e32151ed2b168d6070ff5a37bdf
mahiro-takayama <jinguutakamaro@yahoo.co.jp>
2020-05-19 21:45:13 +0900

    initial commit

---
+++ config/application.rb
@@ -0,0 +1,11 @@
+
+moudule ChatSpace
+  class Application < Rails::Application
+    config.generators do |g|
+      g.stylesheets false
+      g.javascripts false
+      g.helper false
+      g.test_framework false
+    end
+  end
+end
