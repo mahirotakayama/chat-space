71d3e4379431627f3d6c5974d80abfc68d4d9fe9
mahiro-takayama <jinguutakamaro@yahoo.co.jp>
2020-06-28 17:00:49 +0900

    chat-space EC2

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,5 +1,6 @@
 # server-based syntax
 # ======================
+# Defines a single server with a list of roles and multiple properties.
 # You can define all roles on a single server, or split them:
 
 # server "example.com", user: "deploy", roles: %w{app db web}, my_property: :my_value
